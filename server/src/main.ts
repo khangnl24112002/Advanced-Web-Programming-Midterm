c649e2f3772f1c2fa5837d464e0bd6722e0a6102
ptnminh <minhphan113@gmail.com>
2023-11-05 18:48:59 +0700

    feat: add swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,10 +2,11 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { CORS } from './common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix(process.env.GLOBAL_PREFIX || 'v1');
   app.enableCors(CORS);
 
   const port = process.env.PORT || 3000;
@@ -15,6 +16,12 @@ async function bootstrap() {
     }),
   );
   app.use(cookieParser());
+  const options = new DocumentBuilder()
+    .setTitle('API docs')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/swagger', app, document);
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${process.env.GLOBAL_PREFIX}`,
