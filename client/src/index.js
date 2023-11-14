8a3c26800dbb4ff78db5f031412a03dac6abf2e5
Michael Thinh <michaelthinh1008@gmail.com>
2023-11-14 08:54:53 +0700

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./hooks/useAuth";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
             <AuthProvider>
                 <App />
+                <ToastContainer />
             </AuthProvider>
         </BrowserRouter>
     </React.StrictMode>
