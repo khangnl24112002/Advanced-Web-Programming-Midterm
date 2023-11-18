a8742f829753c968f08ff57148a7df1446ae65a8
Khang Nguyen Nhat <khangnl24112002@gmail.com>
2023-11-18 21:00:07 +0700

    Fix: navigate to dashboard if user logged in

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -10,49 +10,48 @@ let statusArr = ["Online", "Offline"];
 let avatarArr = ["/assets/male-avatar.jpg", "/assets/female-avatar.jpg"];
 
 const randomStatus = () =>
-    statusArr[Math.floor(Math.random() * statusArr.length)];
+  statusArr[Math.floor(Math.random() * statusArr.length)];
 
 const randomAvatar = () =>
-    avatarArr[Math.floor(Math.random() * avatarArr.length)];
+  avatarArr[Math.floor(Math.random() * avatarArr.length)];
 
 const Home = () => {
-    const [users, setUsers] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    useEffect(() => {
-        const fetchUsers = async () => {
-            setIsLoading(true);
-            return await userServices.getAll();
-        };
-        fetchUsers()
-            .then((result) => {
-                setUsers(result.data);
-                setIsLoading(false);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }, []);
-    console.log(users);
-    return (
-        <div className="homeScreen">
-            {isLoading && <LoadingSpinner />}
-            {users && (
-                <div className="userContainer">
-                    {users.map((user) => (
-                        <UserCard
-                            key={user.email}
-                            firstName={user.firstName}
-                            lastName={user.lastName}
-                            status={randomStatus()}
-                            email={user.email}
-                            image={randomAvatar()}
-                        />
-                    ))}
-                </div>
-            )}
-            {!users && <p>User Home Screen</p>}
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      setIsLoading(true);
+      return await userServices.getAll();
+    };
+    fetchUsers()
+      .then((result) => {
+        setUsers(result.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <div className="homeScreen">
+      {isLoading && <LoadingSpinner />}
+      {users && (
+        <div className="userContainer">
+          {users.map((user) => (
+            <UserCard
+              key={user.email}
+              firstName={user.firstName}
+              lastName={user.lastName}
+              status={randomStatus()}
+              email={user.email}
+              image={randomAvatar()}
+            />
+          ))}
         </div>
-    );
+      )}
+      {!users && <p>User Home Screen</p>}
+    </div>
+  );
 };
 
 export default Home;
